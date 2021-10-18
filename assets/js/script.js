cce94b7ecffe4d55db831a46c5b102c02369d2a0
Ami Asokumar <gasokumar@gmail.com>
2021-10-18 19:49:35 -0400

    Typed some pseudocode for js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Will probably need arrays [a,b,c]
+// Will probably need objects [
+
+//Make an array that holds all the questions/answers. For each question, make an object that has a question property, options, property, and answer 
+
+[
+var questionOne = {
+        Question: 'How old are you',
+        Options: ['21,22,23,24' ],
+        Answer: '22'
+}, 
+    // If option chosen = answer, correct. Else, wrong
+    {
+        title: '',
+        answer: '',
+        answer: ''
+    }
+]
