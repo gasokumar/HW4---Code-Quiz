cb8af3f1578642d64bc496822b8f224d019c06ef
Ami Asokumar <gasokumar@gmail.com>
2021-10-20 05:02:39 -0400

    Added a timer and start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 // Focus on html then js next. Can style last
 // Will probably need arrays [a,b,c]
 // Will probably need objects [
+var highScoreButton = docment.querySelector("hsbutton")
+var timeEl = document.querySelector("#timer")
+
+
+
 
 //Make an array that holds all the questions/answers. For each question, make an object that has a question property, options, property, and answer 
 // Once end of array is reached, show ending screen
 [
-var questionOne = {
-        Question: 'How old are you',
-        Options: ['21,22,23,24' ],
-        Answer: '22'
-}, 
-    // If option chosen = answer, correct. Else, wrong
-    {   title: 'a',
-        answer: 'b',
-        answer: 'c'
-    }
-]
+// var questionOne = {
+//         Question: 'How old are you',
+//         Options: ['21,22,23,24' ],
+//         Answer: '22'
+// }, 
+//     // If option chosen = answer, correct. Else, wrong
+//     {   title: 'a',
+//         answer: 'b',
+//         answer: 'c'
+//     }
+// ]
